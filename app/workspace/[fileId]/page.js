6ea049e60dfd6fa02687cc25007abe7746c15372
Basudev <samantabasu.2001@gmail.com>
2024-11-17 21:17:59 +0530

    PDF Viewer done

--- app/workspace/[fileId]/page.js
+++ app/workspace/[fileId]/page.js
@@ -1,20 +1,26 @@
 "use client"
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import WorkspaceHeader from "../_components/WorkspaceHeader";
 import PdfViewer from "../_components/PdfViewer";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 function Workspace(){
     const {fileId} = useParams();
+    const fileInfo=useQuery(api.fileStorage.GetFileRecord,{
+        fileId:fileId
+    })
 
-    const GetFileInfo=()=>{
-        
-    } 
+    useEffect(()=>{
+        console.log(fileInfo);
+    },[fileInfo])
+    
     return(
         <div>
             <WorkspaceHeader/>
 
-            <div>
+            <div className="grid grid-cols-2 gap-5"> 
                 <div>
                     {/* Text Edition */}
 
@@ -22,7 +28,7 @@ function Workspace(){
 
                 <div>   
                     {/* Pdf Viewer */}
-                    <PdfViewer/>
+                    <PdfViewer fileUrl={fileInfo?.fileUrl}/>
                 </div>
             </div>
         </div>
--- app/workspace/_components/PdfViewer.js
+++ app/workspace/_components/PdfViewer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function PdfViewer(){
+function PdfViewer({fileUrl}){
+    console.log(fileUrl);  
     return(
         <div>
-            PdfViewer
+            <iframe src={fileUrl+"#toolbar=0"} height="90vh" width="100%" className="h-[90vh]"/>
         </div>
     )
 }
